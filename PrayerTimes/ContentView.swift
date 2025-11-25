7834ae769f7253b6a65bc5dab9c48766998d3709
Tarek Sakakini <tarek.sakakini@gmail.com>
2025-11-24 23:02:53 -0800

    Initial Commit

---
+++ PrayerTimes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PrayerTimes
+//
+//  Created by Tarek Sakakini on 11/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PrayerTimes/PrayerTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrayerTimesApp.swift
+//  PrayerTimes
+//
+//  Created by Tarek Sakakini on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct PrayerTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
